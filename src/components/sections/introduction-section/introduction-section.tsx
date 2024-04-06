73457a7cd0596bbbc99af360ce0b6edcbe9aee06
Riccardo Nuzzone <riccardonuzz@yahoo.it>
2024-04-06 13:33:24 +0200

    Moved icons to /tech folder

--- src/components/sections/introduction-section/introduction-section.tsx
+++ src/components/sections/introduction-section/introduction-section.tsx
@@ -12,7 +12,13 @@ const IntroductionSection = ({ skills }: IntroductionSectionProps) => {
     return (
         <div className="grid grid-cols-12 xl:gap-12 items-center justify-center">
             <div className="col-span-5 max-xl:col-span-12 max-xl:pb-10 flex justify-center items-center text-center">
-                <video
+                <Image
+                    src={'/fedeveloper.gif'}
+                    alt='Front-End Developer'
+                    width={450}
+                    height={450}
+                />
+                {/* <video
                     preload="none"
                     autoPlay
                     muted
@@ -24,14 +30,14 @@ const IntroductionSection = ({ skills }: IntroductionSectionProps) => {
                 >
                     <source src="/fedeveloper.webm" type="video/webm"/>
                     Your browser does not support the video tag.
-                </video>
+                </video> */}
             </div>
             <div className="col-span-7 max-xl:col-span-12">
                 <div className="mb-6">
                     <WindowCard title='introduction' defaultOpen enableControls>
                         <p className="text-white/70">
                             Experienced with all stages of the development cycle for dynamic web projects with <span className="text-white">AGILE methodologies</span> .
-                            Well-versed in numerous programming languages, frameworks and libraries such as <span className="text-white">JavaScript (ES6+) <Image className="inline" width={15} height={15} src='/js.png' alt='JavaScript' /> <Image className="inline" width={15} height={15} src='/ecmascript.png' alt='EcmaScript' />, TypeScript <Image className="inline" width={15} height={15} src='/typescript.svg' alt='TypeScript' />, React <Image className="inline" width={15} height={15} src='/react.png' alt='React' /> and Angular <Image className="inline" width={20} height={20} src='/angular.png' alt='Angular' /></span>.
+                            Well-versed in numerous programming languages, frameworks and libraries such as <span className="text-white">JavaScript (ES6+) <Image className="inline" width={15} height={15} src='/techs/js.png' alt='JavaScript' /> <Image className="inline" width={15} height={15} src='/techs/ecmascript.png' alt='EcmaScript' />, TypeScript <Image className="inline" width={15} height={15} src='/techs/typescript.svg' alt='TypeScript' />, React <Image className="inline" width={15} height={15} src='/techs/react.png' alt='React' /> and Angular <Image className="inline" width={20} height={20} src='/techs/angular.png' alt='Angular' /></span>.
                         </p>
                     </WindowCard>
                 </div>
